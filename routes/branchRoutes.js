489fd9843c0f3dd77361f7023a8dac75aaa00c65
davidecipher <90460745+davidecipher@users.noreply.github.com>
2023-05-27 06:14:02 +0100

    Update branchRoutes.js

--- routes/branchRoutes.js
+++ routes/branchRoutes.js
@@ -4,12 +4,14 @@ const {
     getBranch, 
     getSingleBranch,
     createBranch,
+    editBranchName,
     deleteBranch
 } = require('../controllers/branchControllers');
 
 router.get('/', getBranch);
 router.get('/:id', getSingleBranch);
 router.post('/', createBranch);
+router.put('/:id', editBranchName);
 router.delete('/:id', deleteBranch);
 
 module.exports = router;
