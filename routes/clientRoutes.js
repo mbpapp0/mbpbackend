4a6f27de208f3f1dcd87c6b39a2363a0dbc89b3e
davidecipher <90460745+davidecipher@users.noreply.github.com>
2023-06-15 16:37:01 +0100

    Update clientRoutes.js

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -4,11 +4,13 @@ const {
     getClients,
     getClient,
     addClient,
+    getClientByUser,
     testClient
 } = require('../controllers/clientController');
 
 router.get('/:id', getClients);
 router.get('/client/:id', getClient);
+router.get('/user/:id', getClientByUser);
 router.post('/', addClient);
 router.post('/test', testClient);
 
