401d767240a7e6a3f96e51fd368a095c88a8b12e
davidecipher <90460745+davidecipher@users.noreply.github.com>
2023-06-16 11:02:20 +0100

    Update childrenController.js

--- controllers/childrenController.js
+++ controllers/childrenController.js
@@ -10,8 +10,8 @@ const addChild = async(req, res) => {
     const child = await Child.create(req.body);
 
     try{
-        res.status(200).json(child);
-        
+       // res.status(200).json(child);
+        res.status(200).send(child._id);
     } catch(err){
         res.status(400).json({ error: error.message})
     }
