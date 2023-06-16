f8982f768c474c7afdcf5110f06254c6aff0a9d2
davidecipher <90460745+davidecipher@users.noreply.github.com>
2023-06-16 08:50:33 +0100

    Update clientController.js

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -28,12 +28,14 @@ const getClient = async (req, res) => {
     
 }
 
-const addClient = async (req, res) => {
+const addClient = async (req, res, next) => {
     try {
         const newClient = await Client.create(req.body);
 
         res.status(200).json(newClient);
-    } catch (error) {
+        console.log(newClient.childrenIds);
+        next();
+    } catch (error) {2
         res.status(400).json({error: error.message});
     }
 }
