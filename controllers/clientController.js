80860ba51fd526adbee6551bc217f2cb21200d6d
davidecipher <90460745+davidecipher@users.noreply.github.com>
2023-06-17 19:24:14 +0100

    Update clientController.js

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -1,12 +1,14 @@
 const Client = require('../models/clientModel');
 const Child = require('../models/childrenModel');
 
+// Gets all the IEG Forms from a specific Branch
 const getClients = async (req, res) => {
     const clients = await Client.find({ branch: req.params.id});
 
     res.status(200).json(clients);
 }
 
+// Get From depending on the user who submitted
 const getClientByUser = async (req, res) => {
     try {
         const client = await Client.find({ userID: req.params.id });
@@ -18,6 +20,7 @@ const getClientByUser = async (req, res) => {
     
 }
 
+// Get a specific form by its unique identifer
 const getClient = async (req, res) => {
     try {
         const clients = await Client.find({ _id: req.params.id });
@@ -29,6 +32,7 @@ const getClient = async (req, res) => {
     
 }
 
+// Creates a new form
 const addClient = async (req, res, next) => {
     try {
         const newClient = await Client.create(req.body);
@@ -40,6 +44,8 @@ const addClient = async (req, res, next) => {
     }
 }
 
+
+// Approves a submitted form
 const editClient = async(req, res) => {
     
       
@@ -59,6 +65,7 @@ const editClient = async(req, res) => {
     }
 }
 
+// Test function
 const testClient = async (req, res, next) => {
     console.log(req.body);
     next();
