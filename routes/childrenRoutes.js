6104d3a094c6c0245a0973e8e6d6f37fd9271bfb
davidecipher <90460745+davidecipher@users.noreply.github.com>
2023-06-17 19:21:51 +0100

    Update childrenRoutes.js

--- routes/childrenRoutes.js
+++ routes/childrenRoutes.js
@@ -2,12 +2,10 @@ const express = require('express');
 const router = express.Router();
 const {
     getChildren,
-    addChild,
-    approveChild
+    addChild
 } = require('../controllers/childrenController')
 
 router.get('/:id', getChildren);
 router.post('/', addChild);
-router.put('/approve/:id', approveChild)
 
 module.exports = router;
