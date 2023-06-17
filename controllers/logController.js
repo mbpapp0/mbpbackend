879ee16873ba1530e65860b70e11848381f13ca6
davidecipher <90460745+davidecipher@users.noreply.github.com>
2023-06-17 19:26:08 +0100

    Update logController.js

--- controllers/logController.js
+++ controllers/logController.js
@@ -1,5 +1,6 @@
 const Log = require('../models/logModel');
 
+// Gets logs for every user login
 const getLog = async (req, res) => {
     const logs = await Log.find({ user: req.params.id}).sort({ createdAt: -1});
 
