ca4e7bbdda305ce5253eac8c3a1f17b9edb986b8
davidecipher <90460745+davidecipher@users.noreply.github.com>
2023-10-03 00:26:08 +0100

    Update branchModel.js

--- models/branchModel.js
+++ models/branchModel.js
@@ -4,7 +4,7 @@ const branchSchema = new mongoose.Schema({
     name: {
         type: String, 
         required: true,
-        unique: true,
+        unique: [true, 'Branch Already Exists'],
         uniqueCaseInsensitive: true,
         uniqueErrorMsg: 'Branch Already Exists'
     }
